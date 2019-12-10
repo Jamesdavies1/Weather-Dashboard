d1e045ac6bf2829940a976305360ab07c35fb576
james davies <james.davies92@icloud.com>
2019-12-10 17:29:21 +0000

    added more styling inc div boxes for 5 day planner and wallpaper backgrounds. also inc some fancy txt imgs

--- script.js
+++ script.js
@@ -3,38 +3,43 @@
 // Then dynamicaly generating buttons for each movie in the array
 // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
 
-
 // Generate a reference to our location input field
 var $inputField = $("#city-input");
 
-    // Add an event listener to the 'check weather button' that will make an AJAX call
+// Add an event listener to the 'check weather button' that will make an AJAX call
 var $weatherBtn = $("#add-city");
-$weatherBtn.click(function(event){
-    event.preventDefault()
+$weatherBtn.click(function (event) {
+  event.preventDefault()
 
-    //given input field in index.html a variable
-   var userCity = $inputField.val()
+  //given input field in index.html a variable
+  var userCity = $inputField.val()
 
-    //api address + inputted city
-   var queryUrl = "https://api.openweathermap.org/data/2.5/weather?APPID=81fefb486ea6ff0b0adaaf2f65825e9f&q=" + userCity + "&units=metric";
+  //api address + inputted city
+  var queryUrl = "https://api.openweathermap.org/data/2.5/weather?APPID=81fefb486ea6ff0b0adaaf2f65825e9f&q=" + userCity + "&units=metric";
 
-    //first ajax call to receive weather info of inputted city
-   $.ajax({
-       url: queryUrl,
-       method: "GET"
-   }).then(function(response){
-/*     if (history.indexOf(userCity) === -1) {
-        history.push(userCity);
-        window.localStorage.setItem("history", JSON.stringify(history)); */
-  
-        //makeRow(searchValue);
-      //}
-       //our console.log response from api server displaying info we have requested 
-       console.log("here is the cities weather info: ", response);
-       // console.log("i am her: ", response.name);
+  //var newUrl = "https://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml";
+  //console.log(newUrl)
 
-currentWeather(response);
-   })
+
+  //first ajax call to receive weather info of inputted city
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  }).then(function (response) {
+    if (history.indexOf(userCity) === -1) {
+      history.push(userCity);
+      window.localStorage.setItem("history", JSON.stringify(history));
+
+      makeRow(searchValue);
+    }
+    //our console.log response from api server displaying info we have requested 
+    console.log("here is the cities weather info: ", response);
+    // console.log("i am her: ", response.name);
+
+    currentWeather(response);
+  })
+
+
 });
 //-----------------------------------------------------------------------------------------------------------------------------
 
@@ -46,9 +51,10 @@ currentWeather(response);
 //function addCity
 
 // get current history, if any
+
 /* var history = JSON.parse(window.localStorage.getItem("history")) || [];
 if (history.length > 0) {
-  currentWeather(history[history.length-1]);
+ currentWeather(history[history.length-1]);
 }
 for (var i = 0; i < history.length; i++) {
   makeRow(history[i]);
@@ -56,21 +62,21 @@ for (var i = 0; i < history.length; i++) {
 
 $(".history").on("click", "li", function() {
     currentWeather($(this).text());
-  }); */
+  });  */
 
 //-------------------------block 2- displayed current city weather info-------------------------------------------------------
-function currentWeather(response){
-    var cityName = response.name;
-    $("#name").text("City name: " + cityName);
-    var cityTemp = response.main.temp;
-    $("#temperature").text("City temperature: " + cityTemp + "˚C");
-    var cityHumidity = response.main.humidity;
-    $("#humidity").text("City humidity: " + cityHumidity + "%");
-    var cityWind = response.wind.speed;
-    $("#windspeed").text("City wind: " + cityWind + " MPH");
-    var cityUv = response.main.humidity;
-    $("#uvindex").text("City UV index: " + cityUv + " needs fix");
-    console.log(response);
+function currentWeather(response) {
+  var cityName = response.name;
+  $("#name").text("City name: " + cityName);
+  var cityTemp = response.main.temp;
+  $("#temperature").text("City temperature: " + cityTemp + "˚C");
+  var cityHumidity = response.main.humidity;
+  $("#humidity").text("City humidity: " + cityHumidity + "%");
+  var cityWind = response.wind.speed;
+  $("#windspeed").text("City wind: " + cityWind + " MPH");
+  var cityUv = response.main.humidity;
+  $("#uvindex").text("City UV index: " + cityUv + " needs fix");
+  console.log(cityName);
 
 }
 
@@ -96,6 +102,7 @@ function currentWeather(response){
 //function addCity
 
 // get current history, if any
+
 /* var history = JSON.parse(window.localStorage.getItem("history")) || [];
 if (history.length > 0) {
   currentWeather(history[history.length-1]);
@@ -106,4 +113,17 @@ for (var i = 0; i < history.length; i++) {
 
 $(".history").on("click", "li", function() {
     currentWeather($(this).text());
-  }); */
+  });  */
+
+
+
+
+
+  //NEED HELP WITH
+  
+    //CREATING CITY BUTTONS
+    //FINDING UV FROM API
+    //CREATING 2ND AJAX CALL
+    //FINDING 5 DAY FORECASTS
+
+    //.MOMENT
